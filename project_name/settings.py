288eb9df60e8198b6698919c66291b7fe867cf0c
Andre Sencioles <asenci@gmail.com>
2014-01-25 13:31:51 -0200

    uWSGI and logging optimizations, DJANGO_DEBUG environment variable

--- project_name/settings.py
+++ project_name/settings.py
@@ -1,7 +1,10 @@
 """Django settings module"""
 from os import environ, path
 
+DEBUG = TEMPLATE_DEBUG = environ.get('DJANGO_DEBUG', '0') == '1'
 PROJECT_ROOT = environ.get('DJANGO_HOME', '')
+
+ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
@@ -9,8 +12,6 @@ DATABASES = {
         'NAME': path.join(PROJECT_ROOT, 'default.db'),
     },
 }
-
-DEBUG = TEMPLATE_DEBUG = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_SUBJECT_PREFIX = '[{{ project_name }}] '
@@ -31,28 +32,66 @@ LOGGING = {
     'version': 1,
     'disable_existing_loggers': True,
 
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+
     'formatters': {
         'default': {
             'format': '%(asctime)s <%(name)s:%(levelname)s> %(message)s'
+        },
+        'debug': {
+            'format': '%(asctime)s <%(name)s:%(levelname)s>'
+                      ' [%(module)s:%(process)d:%(thread)d] %(message)s'
         },
     },
 
     'handlers': {
         'console': {
-            'level': 'DEBUG',
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
             'class': 'logging.StreamHandler',
-            'formatter': 'default'
+            'formatter': 'default',
+        },
+        'default': {
+            'level': 'DEBUG' if DEBUG else 'INFO',
+            'class': 'logging.FileHandler',
+            'formatter': 'debug' if DEBUG else 'default',
+            'filename': path.join(PROJECT_ROOT, '{{ project_name }}.log'),
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler',
+            'include_html': True,
         },
     },
 
     'loggers': {
+        '{{ project_name }}': {
+            'handlers': ['console', 'default'],
+            'level': 'DEBUG' if DEBUG else 'INFO',
+        },
         'django': {
-            'handlers': ['console'],
+            'handlers': ['console', 'default'],
+            'level': 'DEBUG' if DEBUG else 'INFO',
+        },
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        'django.security': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
         },
         'py.warnings': {
-            'handlers': ['console'],
-        },
-        'spyne': {
             'handlers': ['console'],
         },
     },
--- project_name/settings_prod.py
+++ project_name/settings_prod.py
@@ -4,10 +4,6 @@ ADMINS = MANAGERS = (
     ('admin', 'root@localhost')
 )
 
-ALLOWED_HOSTS = ['*']
-
-DEBUG = TEMPLATE_DEBUG = False
-
 DEFAULT_FROM_EMAIL = SERVER_EMAIL = 'webmaster@localhost'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
@@ -16,7 +12,5 @@ EMAIL_HOST = 'localhost'
 #EMAIL_HOST_PASSWORD =
 #EMAIL_PORT = 587
 #EMAIL_USE_TLS = True
-
-LOGGING = None
 
 SECRET_KEY = environ.get('DJANGO_SECRET', SECRET_KEY)
